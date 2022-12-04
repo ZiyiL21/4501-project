b5116059eed71a18821a52b591283ae795e07567
ZiyiL21 <117127468+ZiyiL21@users.noreply.github.com>
2022-12-03 23:07:19 -0500

    Delete queries directory

--- queries/popular_days_of_uber.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT strftime("%w", pickup_datetime) AS days,
-COUNT(*) AS day_frequency
-FROM uber_trips
-GROUP BY days
-ORDER BY day_frequency DESC;
-
--- queries/popular_hours_of_taxi.sql
+++
@@ -1,6 +0,0 @@
-
-SELECT strftime("%H", pickup_datetime) AS hours,
-COUNT(*) AS hour_frequency
-FROM taxi_trips
-GROUP BY hours
-ORDER BY hour_frequency desc
